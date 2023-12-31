c735f6fd1e6e2f61efaa84ea2d991ec9a10f25a0
William Arasirwa <mageboarasirwa03@gmail.com>
2023-12-31 14:33:52 +0300

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
+    countdown = 10  
+    while countdown > 0 :
+        print(countdown)
+        countdown -=1
+        print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    return [i ** 2 for i in int_list]
+
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
     pass
